bd553c0684776409f4ca1fd824162fb9e4bef8cc
codeperfectplus <deepak008@live.com>
2025-05-15 21:10:51 +0530

    Update __init__.py and processor.py to include mind map generation functionality

--- transmeet/__init__.py
+++ transmeet/__init__.py
@@ -2,7 +2,8 @@ from transmeet.processor import (
     transcribe_audio_file, 
     generate_meeting_minutes_from_transcript, 
     generate_podcast_script_from_transcript, 
-    synthesize_podcast_audio
+    synthesize_podcast_audio,
+    generate_mind_map_from_transcript
 )
 
 __all__ = [
@@ -10,4 +11,5 @@ __all__ = [
     "generate_meeting_minutes_from_transcript",
     "generate_podcast_script_from_transcript",
     "synthesize_podcast_audio",
+    "generate_mind_map_from_transcript"
 ]
